70141605926ea911f5b3d1a8a63b716897877865
Yusuf Ejaz <129557540+YusufEjaz@users.noreply.github.com>
2025-03-03 13:14:10 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // optional for global styles
+import './index.css';
+import ChartGenerator from './components/ChartGenerator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChartGenerator />
   </React.StrictMode>
 );
