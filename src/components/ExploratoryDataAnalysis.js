b6f8459e78b555c5550929ae4d80518a787dacc1
Yusuf Ejaz <129557540+YusufEjaz@users.noreply.github.com>
2025-03-05 14:10:46 -0800

    Update ExploratoryDataAnalysis.js

--- src/components/ExploratoryDataAnalysis.js
+++ src/components/ExploratoryDataAnalysis.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { /api/generate-eda } from 'react-router-dom';
 
 const ExploratoryDataAnalysis = () => {
   const [csvFile, setCsvFile] = useState(null);
