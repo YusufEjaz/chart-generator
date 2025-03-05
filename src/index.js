a0c08d0aa37113af3958c46fea67c20c06c60d37
Yusuf Ejaz <129557540+YusufEjaz@users.noreply.github.com>
2025-03-05 14:45:07 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import ChartGenerator from './components/ChartGenerator';
-import ExploratoryDataAnalysis from './components/ExploratoryDataAnalysis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<ChartGenerator />} />
-        <Route path="/eda" element={<ExploratoryDataAnalysis />} />
-      </Routes>
-    </Router>
+    <ChartGenerator />
   </React.StrictMode>
 );
